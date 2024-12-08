34ae0b1c1dd3fd037c9e924c2a589469268bf73e
smartflutist661 <smartflutist661@gmail.com>
2024-12-08 15:16:19 -0500

    Attempt some uv improvements

--- readerware_browser/__main__.py
+++ readerware_browser/__main__.py
@@ -1,4 +1,9 @@
 from readerware_browser.app import APP
 
-if __name__ == "__main__":
+
+def main() -> None:
     APP.run(host="127.0.0.1", port=8080, debug=True)
+
+
+if __name__ == "__main__":
+    main()
